5aeea23b0a5053e74a5950e1ad59e6625d7986b3
Hynek Lukeš <hyneklu@gmail.com>
2022-12-11 21:24:47 +0100

    whitelist,exception handeling,startingZero

--- standalone/gpxClassy.py
+++ standalone/gpxClassy.py
@@ -28,11 +28,22 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.parent = parent
     #Loading config and settings files
-        with open(os.path.join(sys.path[0],"key.json"),"r",encoding="UTF-8") as keyfile:
-            key_raw = keyfile.read() 
-        apiKey_raw = json.loads(key_raw)
-        apiKey =apiKey_raw["key"]
-    
+        
+        #Loading the keyfile
+
+        keyfile_path = os.path.join(sys.path[0],"key.json")
+        try:
+            with open(keyfile_path,"r",encoding="UTF-8") as keyfile:
+                key_raw = keyfile.read() 
+                apiKey_raw = json.loads(key_raw)
+                apiKey = apiKey_raw["key"]
+        except FileNotFoundError as err:
+            print(f"The keyfile at {keyfile_path} was not found, please provide a valid key file.")
+            raise
+        except Exception as err:
+            print(f"Unexpected {err}, {type(err)}")
+            raise
+        
     #functions
         #GPG function and two start functions - Tkinter button called, check directory function
         def CheckDir(path):
@@ -168,8 +179,16 @@ class MainApplication(tk.Frame):
             city2 = str(json_r2['locality'])
             data_points = data.points[0]
             year = str(data_points.time.year)
-            month = str(data_points.time.month)
-            day = str(data_points.time.day)
+            month_raw = str(data_points.time.month)
+            if (int(month_raw) < 10):
+                month = "".join(("0",month_raw))
+            else:
+                month = month_raw
+            day_raw = str(data_points.time.day)
+            if (int(day_raw) < 10):
+                day = "".join(("0",month_raw))
+            else:
+                day = day_raw
             if data_points.time.minute<10:
                 start_time = (str(data_points.time.hour)+"∶0"+str(data_points.time.minute))
             else:
@@ -253,11 +272,11 @@ class MainApplication(tk.Frame):
                             nur = nur.replace(i.rep,empt)
             gpx_file.close()
             final_name_raw = nur
-            whitelist = set(string.ascii_letters + string.digits + "!_-()ěščřžýáíéĚŠČŘŽÝÁÍÉÚŮúůťď∶ ")
+            whitelist = set(string.ascii_letters + string.digits + "!_-()#ÄäÜüÖößèěščřžýáíéĚŠČŘŽÝÁÍÉÚŮúůťď∶ " + "ąęćłńśźż")    #whitelist for the generated file name, anything not here will not come through
             final_name_raw = ''.join(c for c in final_name_raw if c in whitelist)
             nur = final_name_raw
 
-            if (manual_move == False) or (manual == False):
+            if (manual_move == False) or (os.path.exists(manual) == False):
                 while (final_name_raw+".gpx") in os.listdir(outp):
                         y = y+1
                         final_name_raw = nur+"_"+str(y)
@@ -265,7 +284,7 @@ class MainApplication(tk.Frame):
                 shutil.move(in_path_file,(outp+"\\"+final_name_raw))
             else:
                 #Problem, os.listdir not working without manual % should not be a problem
-
+                
                 while (final_name_raw+".gpx") in os.listdir(manual):
                         y = y+1
                         final_name_raw = nur+"_"+str(y)
